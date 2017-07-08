5808cead09e6a84890cd009bfac436dd9336ee88
Samidha Verma <samidha1705@gmail.com>
2017-07-08 18:55:20 +0530

    Added highscore system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,10 +12,14 @@ public class GameManager : MonoBehaviour {
 
 	private PlatformDestructor[] platformList;
 
+	private ScoreManager theScoreManager;
+
 	// Use this for initialization
 	void Start () {
 		platformStartPoint = platformGenerator.position;
 		playerStartPoint = thePlayer.transform.position;
+
+		theScoreManager = FindObjectOfType<ScoreManager> ();
 	}
 	
 	// Update is called once per frame
@@ -29,6 +33,7 @@ public class GameManager : MonoBehaviour {
 
 	public IEnumerator RestartGameCo () {
 
+		theScoreManager.scoreIncreasing = false;
 		thePlayer.gameObject.SetActive (false);
 		yield return new WaitForSeconds (0.5f);
 		platformList = FindObjectsOfType<PlatformDestructor> ();
@@ -41,6 +46,9 @@ public class GameManager : MonoBehaviour {
 		thePlayer.transform.position = playerStartPoint;
 		platformGenerator.position = platformStartPoint;
 		thePlayer.gameObject.SetActive (true);
+
+		theScoreManager.scoreCount = 0;
+		theScoreManager.scoreIncreasing = true;
 	}
 }
 
